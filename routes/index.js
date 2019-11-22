0d0721e69262e0f157fb40d7b6b36cdf9f84f0f7
Dominic Pham <36905285+dpham123@users.noreply.github.com>
2019-11-21 17:49:09 -0800

    [DVR2-20] Add Function for Retrieving List of Videos from DynamoDB

--- routes/index.js
+++ routes/index.js
@@ -6,34 +6,10 @@ const util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const randomUUID = uuidv4()
-  
-  const vidURL1 = util.format('test_url_1/%s', randomUUID)
-  const vidURL2 = util.format('test_url_2/%s', randomUUID)
-
-  const vidTitle1 = util.format('test_title_1/%s', randomUUID)
-  const vidTitle2 = util.format('test_title_2/%s', randomUUID)
-
-  const response = {
-    "user_uuid": randomUUID,
-    "videos": [
-      {
-        "url": vidURL1,
-        "video_title": vidTitle1
-      },
-      {
-        "url": vidURL2,
-        "video_title": vidTitle2
-      }
-    ]
-  }
-
-  dynamo.createUserEntry(randomUUID).then(() => {
-    dynamo.addVideo(randomUUID, vidURL1, vidTitle1)
-    dynamo.addVideo(randomUUID, vidURL2, vidTitle2)
+  dynamo.getVideos('4STmPwrY4zUX3lg7iwmHvmZsO2F2').then(data => {
+    console.log(data)
+    res.sendStatus(200)
   })
-  
-  res.json(response)
 });
 
 module.exports = router;
