55f860bd40ef8993eef972878b2d81905a81eeca
Dominic Pham <36905285+dpham123@users.noreply.github.com>
2019-11-22 17:03:42 -0800

    [No-ZenHub] CORS code

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ var uploadRouter = require('./routes/upload');
 var signupRouter = require('./routes/signup');
 var videoRouter = require('./routes/videos');
 var app = express();
+var cors = require('cors');
+app.use(cors())
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
 
